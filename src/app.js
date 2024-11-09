37f3fcf27e7870c13ab156c7fd6fa5dc313f4e24
MARA\marik <marikompar@gmail.com>
2024-11-09 14:51:06 +0100

    new

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@ const fs = require('fs');
 const https = require('https');
 const { Pool } = require('pg');
 require('dotenv').config();
+const externalURL = process.env.RENDER_EXTERNAL_URL; 
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')); 
+
+const config = {
+    baseURL: externalUrl || `https://localhost:${port}`, 
+};
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -137,14 +143,20 @@ app.get('/fetch-data', async (req, res) => {
     }
 })
 
-const port = 3001;
-
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-    }, app)
-    .listen(port, function () {
-    console.log(`Server running at https://localhost:${port}/`);
-});
+if (externalURL) { 
+    const hostname = '0.0.0.0'; 
+    app.listen(port, hostname, () => { 
+      console.log(`Server locally running at http://${hostname}:${port}/ and from 
+  outside on ${externalURL}`); 
+    }); 
+} else { 
+    https.createServer({ 
+    key: fs.readFileSync('server.key'), 
+    cert: fs.readFileSync('server.cert') 
+    }, app) 
+    .listen(port, function () { 
+    console.log(`Server running at https://localhost:${port}/`); 
+    }); 
+} 
 
 
